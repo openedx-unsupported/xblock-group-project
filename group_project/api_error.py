d7f6171941067dc3c73ca2a13c7acb23d9e745fd
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-02-01 12:42:32 -0200

    Switch to ugettext_lazy to avoid issues on module load time
    Signed-off-by: Giovanni Cimolin da Silva <giovannicimolin@gmail.com>

--- group_project/api_error.py
+++ group_project/api_error.py
@@ -1,7 +1,7 @@
 import json
 from urllib2 import HTTPError
 
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 ERROR_CODE_MESSAGES = {}
